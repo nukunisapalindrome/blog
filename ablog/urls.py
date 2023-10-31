933a6de986b1ea4d7642c687c99b41ff352cc614
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-10-31 21:36:11 +0700

    try

--- ablog/urls.py
+++ ablog/urls.py
@@ -5,11 +5,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [
+try :
+    urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+except:
+    urlpatterns =[]

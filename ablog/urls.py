64cdeece4727931a74b48984ff58c9387074cee1
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-10-31 21:48:35 +0700

    delete try and except

--- ablog/urls.py
+++ ablog/urls.py
@@ -4,23 +4,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', include('theblog.urls')),
+path('members/', include('django.contrib.auth.urls')),
+path('members/', include('members.urls')),
+path('ckeditor/', include('ckeditor_uploader.urls')),
 
-try:
-    urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('theblog.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-except :
-    urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('theblog.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

b89e93c49146b1219dbfa632495937ed84f14134
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-10-31 21:44:37 +0700

    try block

--- ablog/urls.py
+++ ablog/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-try :
+try:
     urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
@@ -14,5 +14,13 @@ try :
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-except:
-    urlpatterns =[]
+
+except :
+    urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('theblog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

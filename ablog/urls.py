ffbe36bd2eccf0582b8343ea0112a45e4d8c0623
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-10-31 23:30:09 +0700

    delete url stuff

--- ablog/urls.py
+++ ablog/urls.py
@@ -5,9 +5,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('', include('theblog.urls')),
-path('members/', include('django.contrib.auth.urls')),
-path('members/', include('members.urls')),
-path('ckeditor/', include('ckeditor_uploader.urls')),]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('theblog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),]
+    # ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

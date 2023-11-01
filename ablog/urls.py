5111ffdece7ade890c683223a8a09c886549147c
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-11-01 19:23:29 +0700

    change url settings for 4.0

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('ckeditor/', include('ckeditor_uploader.urls')),]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

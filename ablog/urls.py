ec3a9eb043af35bc3aaf613cb57cc2fba8b0ddd0
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-11-01 19:43:03 +0700

    try comment out path url

--- ablog/urls.py
+++ ablog/urls.py
@@ -8,11 +8,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
-    path('members/', include('django.contrib.auth.urls')),
+    # path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-    # urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

356c107e9d6c8cacb150a62128e209b543d500fb
Nukun Ukritnukun <n.ukritnukun@gmail.com>
2023-11-01 19:36:51 +0700

    comment out DEBUG in URL

--- ablog/urls.py
+++ ablog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+    # urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
